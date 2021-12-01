a4213750aff35522f5c8c2b4e108fb19d61bbb50
leonaasanova <asanova_l@auca.kg>
2021-12-02 00:20:08 +0600

    commit

--- app/src/main/java/kg/test/app_project2/home.kt
+++ app/src/main/java/kg/test/app_project2/home.kt
@@ -3,9 +3,9 @@ package kg.test.app_project2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_home)
     }
 }
