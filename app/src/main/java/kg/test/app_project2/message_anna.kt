e601be2502063acebc775ae710c99d629f84a323
leonaasanova <asanova_l@auca.kg>
2021-12-05 00:09:40 +0600

    commit

---
+++ app/src/main/java/kg/test/app_project2/message_anna.kt
@@ -0,0 +1,11 @@
+package kg.test.app_project2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class message_anna : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message_anna)
+    }
+}
